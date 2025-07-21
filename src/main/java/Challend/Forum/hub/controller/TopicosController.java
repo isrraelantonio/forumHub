dd8b18e0b85fc246f129ccc75521873f5393252c
Israel Antonio <israelantonio027@gmail.com>
2025-07-21 17:31:54 -0300

    Atualização da classe TopicoController
    Ajuste do tipo de objeto que será passado para o nosso controller do tipo POST e  injeção da classe de serviço CriarTopicos para o tratamento dos dados recebidos.

--- src/main/java/Challend/Forum/hub/controller/TopicosController.java
+++ src/main/java/Challend/Forum/hub/controller/TopicosController.java
@@ -1,18 +1,27 @@
 package Challend.Forum.hub.controller;
 
 
-import Challend.Forum.hub.domain.topico.DadosTopico;
+import Challend.Forum.hub.domain.curso.CursoRepository;
+import Challend.Forum.hub.domain.topico.*;
+import Challend.Forum.hub.domain.usuario.UsuarioRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/topico")
 public class TopicosController {
 
+    @Autowired
+    private  CriarTopicos criarTopicos;
+
+
+    @Transactional
     @PostMapping
-    public DadosTopico primeiraRequest(@RequestBody @Valid DadosTopico dados){
-        var topico = new DadosTopico(dados.titulo(), dados.mensagem(),dados.data(),dados.autor(), dados.curso());
-        return topico;
+    public DetalhamentoTopico primeiraRequest(@RequestBody @Valid DadosTopico dados){
+        var topicoEmSi = criarTopicos.criandoTopico(dados);
+        return topicoEmSi;
     }
 
 }

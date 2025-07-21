104121a9f5bca675a4fc34e9864d64a05bd37a3c
Israel Antonio <israelantonio027@gmail.com>
2025-07-21 17:23:14 -0300

    Criação da nossa classe de serviço CriarTopicos
    Na mesma fazemos verificações de usuário/autor e curso, quanto suas presenças em seus respectivos bancos de dados antes da criação do tópico.

---
+++ src/main/java/Challend/Forum/hub/domain/topico/CriarTopicos.java
@@ -0,0 +1,57 @@
+package Challend.Forum.hub.domain.topico;
+
+
+import Challend.Forum.hub.ValidacaoException;
+import Challend.Forum.hub.domain.curso.CursoRepository;
+import Challend.Forum.hub.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CriarTopicos {
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+
+
+
+    public DetalhamentoTopico criandoTopico(DadosTopico dados){
+
+
+        if(!usuarioRepository.existsById(dados.autor())){
+            throw  new ValidacaoException("o autor informado não existe!");
+
+        }
+        if(dados.curso() != null && !cursoRepository.existsById(dados.curso())){
+            throw  new ValidacaoException("O curso informado não existe!");
+        }
+
+
+//        validadores.forEach(v-> v.validar(dados));
+        var usuario = usuarioRepository.getReferenceById(dados.autor());
+        var curso = cursoRepository.getReferenceById(dados.curso());
+        var topico = new Topico(dados.titulo(), dados.mensagem(), usuario, curso);
+        topicoRepository.save(topico);
+
+
+
+
+        return new DetalhamentoTopico(topico);
+
+
+
+    }
+
+
+
+
+
+
+}

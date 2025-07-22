e6de54df282aac5ec59ce86e92205e957e939269
Israel Antonio <israelantonio027@gmail.com>
2025-07-22 20:25:31 -0300

    Criação de novas requests e queries na interface Topicorepository
    Em nosso controller definimos três novas rotas, todas para detalhamento de tópicos onde duas dessas com pesquisas mais específicas, uma com relação a curso e outra passando o curso e  o ano nas requisições.

--- src/main/java/Challend/Forum/hub/controller/TopicosController.java
+++ src/main/java/Challend/Forum/hub/controller/TopicosController.java
@@ -1,13 +1,16 @@
 package Challend.Forum.hub.controller;
 
 
-import Challend.Forum.hub.domain.curso.CursoRepository;
 import Challend.Forum.hub.domain.topico.*;
-import Challend.Forum.hub.domain.usuario.UsuarioRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/topico")
@@ -16,6 +19,9 @@ public class TopicosController {
     @Autowired
     private  CriarTopicos criarTopicos;
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
 
     @Transactional
     @PostMapping
@@ -23,5 +29,45 @@ public class TopicosController {
         var topicoEmSi = criarTopicos.criandoTopico(dados);
         return topicoEmSi;
     }
+
+
+// Esse é apenas um exemplo em caso que se quisesse apenas exibir uma lista de tópicos.
+//   @GetMapping
+//   public List<DetalhamentoTopico> listagemDeTopicos(){
+//        var detalhesTopico = topicoRepository.findByEstadoDoTopicoTrue().stream()
+//                 .map(DetalhamentoTopico::new).toList();
+//        return detalhesTopico;
+//
+//   }
+
+// Esa é a forma de retornar uma lista do tipo  page no formato JSON
+    @GetMapping
+    public Page<DetalhamentoTopico> listagemDeTopicos(@PageableDefault(sort = "dataCriacao") Pageable paginacao){
+        var paginacaodetalhesTopico = topicoRepository.findByEstadoDoTopicoTrue(paginacao)
+                .map(DetalhamentoTopico::new);
+
+        return paginacaodetalhesTopico;
+
+    }
+
+
+    @GetMapping("/{curso}")
+    public Page<DetalhamentoTopico> listagemDeTopicosCurso(@PageableDefault(sort = "dataCriacao") Pageable paginacao,@PathVariable String curso){
+        var paginacaodetalhesTopico = topicoRepository.topicoPeloCurso(paginacao, curso)
+                .map(DetalhamentoTopico::new);
+
+        return paginacaodetalhesTopico;
+
+    }
+
+    @GetMapping("/{curso}/{ano}")
+    public Page<DetalhamentoTopico> topicoPeloano(@PageableDefault(sort = "data_criacao") Pageable paginacao,@PathVariable String ano, @PathVariable String curso){
+        var paginacaodetalhesTopico = topicoRepository.topicoPeloano(paginacao, curso, ano)
+                .map(DetalhamentoTopico::new);
+
+        return paginacaodetalhesTopico;
+
+    }
+
 
 }
--- src/main/java/Challend/Forum/hub/domain/topico/TopicoRepository.java
+++ src/main/java/Challend/Forum/hub/domain/topico/TopicoRepository.java
@@ -1,6 +1,36 @@
 package Challend.Forum.hub.domain.topico;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+//    List<Topico> findByEstadoDoTopicoTrue();
+
+    Page<Topico> findByEstadoDoTopicoTrue(Pageable paginacao);
+
+
+    @Query("""
+            SELECT t FROM Topico t
+            WHERE
+            t.estadoDoTopico = true AND
+            t.curso.id = (
+                SELECT c.id FROM Curso c WHERE c.nome = :nomeCurso
+            )
+            """)
+    Page<Topico> topicoPeloCurso(Pageable paginacao, String nomeCurso);
+
+
+    @Query(value = """
+             SELECT t.* FROM topicos t
+                 JOIN cursos c ON t.curso_id = c.id
+                 WHERE t.estado_do_topico = true
+                 AND YEAR(t.data_criacao) = :ano
+                 AND c.nome COLLATE utf8mb4_general_ci = :nomeCurso
+                  ORDER BY t.data_criacao 
+            """, nativeQuery = true)
+    Page<Topico> topicoPeloano(Pageable paginacao,String nomeCurso, String ano);
 }

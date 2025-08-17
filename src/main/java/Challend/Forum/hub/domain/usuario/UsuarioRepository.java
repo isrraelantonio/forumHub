63f857298e707a573acb9d4def6b50c5af55cca1
Israel Antonio <israelantonio027@gmail.com>
2025-08-16 22:23:04 -0300

    Adição de nova dependência ao pom.xml,  criação de consultas no repositório para validação de usuário.

--- src/main/java/Challend/Forum/hub/domain/usuario/UsuarioRepository.java
+++ src/main/java/Challend/Forum/hub/domain/usuario/UsuarioRepository.java
@@ -1,6 +1,10 @@
 package Challend.Forum.hub.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String username);
+    UserDetails findBySenha(String login);
+
 }
